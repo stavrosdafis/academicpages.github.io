b8847ad6de885795d31471e308ecf12220fa7f9a
Stavros Dafis <sdafis@lmd.polytechnique.fr>
2018-10-24 23:44:02 +0200

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,57 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Paris,France",
+    48.8566,
+    2.3522
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Montpellier, France",
+    43.6108,
+    3.8767
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Barcelona, Spain",
+    41.3851,
+    2.1734
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Athens, Greece",
+    37.9838,
+    23.7275
+  ],
+  [
+    "Trier, Germany",
+    49.7500,
+    6.6371
+  ],
+  [
+    "Mykonos, Greece",
+    37.4467,
+    25.3289
+  ],
+  [
+    "Lecce, Italy",
+    40.3515,
+    18.1750
+  ],
+  [
+    "Montreal, Canada",
+    45.5017,
+    -73.5673
+  ],
+  [
+    "Thessaloniki, Greece",
+    40.6401,
+    22.9444
+  ],
+  [
+    "Vienna, Austria",
+    48.2082,
+    16.3738
+  ],
+  [
+    "Toulouse, France",
+    43.6047,
+    1.4442
   ]
 ];
